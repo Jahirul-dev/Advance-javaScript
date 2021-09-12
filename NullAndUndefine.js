e3d610efb2bbbda1fa5f0cb4b6e80339e53ebb13
Jahirul-dev <jahirul.jitu7@gmail.com>
2021-09-13 00:39:10 +0600

    js

---
+++ NullAndUndefine.js
@@ -0,0 +1,20 @@
+//undefined
+let test;
+console.log(test);
+//one
+function add(a,b){
+}
+const results = add(2,3)
+console.log(results);
+//two
+
+function sub(one, two){
+    console.log(one, two);
+}
+console.log(sub(2));
+//three
+var Obj = {Name: "tesla", age:34, Id:45};
+console.log(Obj.rr);
+
+let arr = [3,4,5,5];
+console.log(arr[12]);
