255c09b0b1a7da27f4303b7270ade659ab7b4e12
Jahirul-dev <jahirul.jitu7@gmail.com>
2025-01-11 18:24:04 +0600

    datas

--- Dom.js
+++ Dom.js
@@ -1,7 +1,7 @@
-let para = document.querySelectorAll("p");
+// let para = document.querySelectorAll("p");
 
-para.forEach((element) => {
-  console.log(element.innerText);
-});
+// para.forEach((element) => {
+//   console.log(element.innerText);
+// });
 
-console.log(para[1].innerText);
+// console.log(para[1].innerText);
