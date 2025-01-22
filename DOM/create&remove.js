96954d29b35598784b1fc02a25b2de726482bb4f
jitu <jahirul.jitu7@gmail.com>
2025-01-22 21:19:42 +0600

    das

--- DOM/create&remove.js
+++ DOM/create&remove.js
@@ -6,22 +6,30 @@
 // //         console.log(e.target);
 
 // //         e.target.style.textDecoration = 'line-through'
-        
+
 // //     })
 // // })
 
 //remove
-let liTag01 = document.querySelectorAll('li');
+let liTag01 = document.querySelectorAll("li");
 
-liTag01.forEach(eachLi01 =>{
-    eachLi01.addEventListener('click', ()=>{
-        eachLi01.remove();
-    })
-})
+liTag01.forEach((eachLi01) => {
+  eachLi01.addEventListener("click", () => {
+    eachLi01.remove();
+  });
+});
 
 //add
-let getDiv = document.querySelector('div');
-let addButton = document.querySelector('button')
-addButton.addEventListener('click', (e)=>{
-        getDiv.innerHTML += '<li>Data added</li>'
-    })
+let getDiv = document.querySelector("div");
+// let addButton = document.querySelector('button')
+// addButton.addEventListener('click', (e)=>{
+//     getDiv.innerHTML += '<li>Data added</li>';
+//     })
+
+let btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+  const addLi = document.createElement("li");
+  addLi.textContent = "Data added dwd";
+  //getDiv.append(addLi);
+  getDiv.prepend(addLi);
+});
