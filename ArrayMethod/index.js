a5636222da37e3ffbcef9b62fba73cbc1d612ae4
Jahirul-dev <jahirul.jitu7@gmail.com>
2025-02-19 22:18:28 +0600

    filter with objects

--- ArrayMethod/index.js
+++ ArrayMethod/index.js
@@ -4,3 +4,18 @@ const filterArray = datas.filter((data)=>{
     return data>=20
 })
 console.log(filterArray);
+
+var user = [
+    {name: 'Syean', premium: true},
+    {name: 'Ryean', premium: false},
+    {name: 'Jyean', premium: true},
+    {name: 'Tyesan', premium: true},
+    {name: 'Byean', premium: false}
+];
+
+let filterusers = user.filter((data1)=>{
+    return data1.premium
+})
+
+console.log(filterusers);
+console.log(user);
