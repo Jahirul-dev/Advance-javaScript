aa4f30ee4d1e3f3d91971a11e4a4d7de1855fb7b
Jahirul-dev <jahirul.jitu7@gmail.com>
2025-02-20 18:16:43 +0600

    map

--- ArrayMethod/map.js
+++ ArrayMethod/map.js
@@ -7,3 +7,23 @@ const mapPrice = prices.map((price)=>{
 console.log(prices);
 console.log(mapPrice);
 
+const products = [
+    {name: 'Gold Star', priceVal:200},
+    {name: 'Diamond Star', priceVal:403},
+    {name: 'Ruby Star', priceVal:3220},
+    {name: 'Monika Star', priceVal:534},
+    {name: 'Silver Star', priceVal:34},
+    {name: 'Titenium Star', priceVal:76}
+]
+
+const saleProducts = products.map((deductVal =>{
+    
+    if (deductVal.priceVal > 50){
+        return {name:deductVal.name, priceVal:deductVal.priceVal/2}
+    }
+    else
+    return deductVal;
+}))
+
+console.log(products);
+console.log(saleProducts);
