e99de8b1c1533a54550e9516c20878b50303fef7
jitu <jahirul.jitu7@gmail.com>
2025-02-25 14:58:20 +0600

    more about reduce

--- reduce.js
+++ reduce.js
@@ -1,2 +1,14 @@
-let arr  = [23,43,23,432]
+//The array reduce in JavaScript is a predefined method used to reduce an array to a single value by passing a callback function on each element of the array.
 
+const arr = [23, 43, 67, 23, 432];
+const acc = 0;
+let arrayReturn = arr.reduce((acc, val) => {
+  acc = acc;
+  if (val > 20) {
+    acc += 1;
+  }
+  console.log(acc);
+
+  return acc;
+});
+console.log(arrayReturn);
